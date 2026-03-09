9d3d1fca394fda3e7f97880c85ea5d34556f18e7
aadharwadvss <aadharwad.vss@gmail.com>
2026-03-09 12:14:05 +0530

    Update _document.js

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,8 +4,17 @@ export default function Document() {
   return (
     <Html lang="mr" suppressHydrationWarning>
       <Head>
+
+        {/* Google AdSense Verification */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7222952470925551"
+          crossOrigin="anonymous"
+        ></script>
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
       </Head>
       <body suppressHydrationWarning>
         <Main />
